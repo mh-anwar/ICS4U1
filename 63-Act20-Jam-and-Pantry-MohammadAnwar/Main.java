340f2d1b63d263a1f1d3070f097bb324b3c501d2
mh-anwar <mohammad.anwar2939@gmail.com>
2023-12-09 00:29:41 -0500

    Add code from Sat, 09 Dec 2023 00:29:41 -0500

---
+++ 63-Act20-Jam-and-Pantry-MohammadAnwar/Main.java
@@ -0,0 +1,71 @@
+class Main {
+    public static void main(String[] args) {
+        // ExpirationTester.run();
+        // JamTester.run();
+        // PantryTester.run();
+
+        Library.print("Part 1");
+
+        Part1.run();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        Library.print("Pantry 2");
+        Pantry2Tester.run();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        Library.print("Pantry 3");
+        Pantry3Tester.run();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        Library.print("Pantry 4");
+        Pantry4Tester.run();
+    }
+}
