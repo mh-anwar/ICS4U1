feaf4e05ca434c74a683eefcebd78555481e1351
mh-anwar <mohammad.anwar2939@gmail.com>
2024-01-18 18:04:46 -0500

    Add code from Thu, 18 Jan 2024 18:04:46 -0500

---
+++ ISU-Group-Project-ics4u-40-buckland-3/CorporationTester.java
@@ -0,0 +1,10 @@
+import Corporation.Corporation;
+import Root.Library;
+
+class CorporationTester {
+    public static void main(String[] args) {
+
+        Corporation evilCorp = new Corporation("Evil Corp.", "Charlie Chaplin");
+        System.out.println(evilCorp);
+    }
+}
