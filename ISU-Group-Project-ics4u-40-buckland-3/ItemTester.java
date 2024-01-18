0b1d83091e6342a4e93212542f3c3ffedb6f7821
mh-anwar <mohammad.anwar2939@gmail.com>
2024-01-17 19:13:56 -0500

    Add code from Wed, 17 Jan 2024 19:13:56 -0500

---
+++ ISU-Group-Project-ics4u-40-buckland-3/ItemTester.java
@@ -0,0 +1,9 @@
+import Item.Item;
+import Root.Library;
+
+class ItemTester {
+    public static void main(String[] args) {
+        Item myItem = new Item("Wood", 15.7, 9.5);
+        System.out.println(myItem);
+    }
+}
